0fa2b2f31fc25eead82d30acebafa0f07c8d890f
Hitansh <hitansh.gupta@kristalball.com>
2023-12-29 14:46:55 +0530

    responseEntity class added

--- src/main/java/com/java/job/app/job/JobController.java
+++ src/main/java/com/java/job/app/job/JobController.java
@@ -1,6 +1,8 @@
 package com.java.job.app.job;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -16,22 +18,22 @@ public class JobController {
 
 
     @GetMapping("/jobs")
-    public List<Job> FindAll(){
-        return jobService.findALL();
+    public ResponseEntity <List<Job>> FindAll(){
+        return ResponseEntity.ok(jobService.findALL());
     }
 
     @PostMapping(path = "/jobs")
-    public String createJob(@RequestBody Job job){
+    public ResponseEntity<String> createJob(@RequestBody Job job){
         jobService.createJob(job);
-        return "Job added SUCCESSFULLY";
+        return new ResponseEntity<>("Job added successfully",HttpStatus.CREATED);
     }
 
     @GetMapping("/jobs/{id}")
-    public Job getJobById(@PathVariable Long id){
+    public ResponseEntity<Job>getJobById(@PathVariable Long id){
         Job job = jobService.getJobById(id);
         if(job!=null)
-            return job;
-        return new Job(1L,"Test job","Test desc","200","300","test location");
+            return new ResponseEntity<>(job, HttpStatus.OK);
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
 }
