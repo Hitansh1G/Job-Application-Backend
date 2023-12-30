b7592dd7d9b96972bf08e5fbd28edd56090830a5
Hitansh <hitansh.gupta@kristalball.com>
2023-12-30 17:53:38 +0530

    defining review controller service and repo

---
+++ src/main/java/com/java/job/app/Review/ReviewService.java
@@ -0,0 +1,4 @@
+package com.java.job.app.Review;
+
+public interface ReviewService {
+}
---
+++ src/main/java/com/java/job/app/Review/impl/ReviewServiceImpl.java
@@ -0,0 +1,8 @@
+package com.java.job.app.Review.impl;
+
+import com.java.job.app.Review.ReviewService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReviewServiceImpl  {
+}
